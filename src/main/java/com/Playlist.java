ae8bd3f851d331cfe60e2f26acef77ec9867ae2f
DemigodKushal <shrivastavakushal3@gmail.com>
2025-03-18 10:17:19 +0530

    Simplified Playlist Class

--- src/main/java/com/Playlist.java
+++ src/main/java/com/Playlist.java
@@ -1,38 +1,22 @@
 package com;
 
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 
 public class Playlist {
-    public File playlist;
+    private File playlist;
     private String name;
-
-    public Playlist(String name){
-//        this.playlist = new File("C:\\playlists:\\"+name+".txt");
-        this.playlist = new File("name.txt");
+    public void setName(String name) {
+        this.name = name;
     }
-
-    public void addSong(String songId){
-        try (FileWriter writer = new FileWriter(name + ".txt", true)) { // 'true' enables append mode
-            writer.write(songId + "\n");
-        } catch (IOException e) {
-            System.out.println("An error occurred: " + e.getMessage());
-
+    public File getPlaylist() {
+        return playlist;
     }
-}
-//    public Song[] getSongsFromPlaylist(String playlistName){}
-
-
-
     public String getName() {
         return name;
     }
-
-    public static void main(String[] args) {
-        Playlist test = new Playlist("test");
-
-
-
+    public void setPlaylist(File playlist) {
+        this.playlist = playlist;
     }
+
+
 }
