f43f247ba86c2406fd095e6dae7f7657621b5df1
DemigodKushal <shrivastavakushal3@gmail.com>
2025-03-18 10:15:13 +0530

    Playlist Handler works now

---
+++ src/main/java/com/playlistHandler.java
@@ -0,0 +1,78 @@
+package com;
+
+import javax.tools.FileObject;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import com.google.gson.*;
+
+public class playlistHandler{
+    public ArrayList<Playlist> playlists = new ArrayList<>();
+    Gson gson = new Gson();
+    public Playlist createPlaylist(String name){
+        File file = new File("C:\\playlists\\" + name);
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            System.out.println("Playlist creation not possible " + e.getMessage());;
+        }
+        Playlist playlist = new Playlist();
+        playlist.setName(name);
+        playlist.setPlaylist(file);
+        return playlist;
+    }
+    public void importPlaylists(){
+        File directory = new File("C:\\playlists\\");
+        File[] files = directory.listFiles();
+        for (File file : files){
+            Playlist playlist  = new Playlist();
+            playlist.setName(file.getName());
+            playlist.setPlaylist(file);
+            playlists.add(playlist);
+        }
+
+    }
+
+    public void addSong(Playlist playlist, Song song){
+        JsonArray jsonfile = new JsonArray();
+        File file = playlist.getPlaylist();
+        Path filePath = file.toPath();
+        JsonObject newSong = new JsonObject();
+        newSong.addProperty("VideoId", song.getVideoId());
+        newSong.addProperty("title", song.getTitle());
+        newSong.addProperty("artist", song.getArtist());
+        newSong.addProperty("duration", song.getDuration());
+        if (Files.exists(playlist.getPlaylist().toPath())){
+            try (Reader reader = Files.newBufferedReader(filePath)) {
+                JsonElement rootElement = JsonParser.parseReader(reader);
+
+                // Check if the file contains a JSON array
+                if (rootElement.isJsonArray()) {
+                    jsonfile = rootElement.getAsJsonArray();
+                } else if (rootElement.isJsonObject()) {
+                    // If it's a single object, convert it into an array
+                    jsonfile.add(rootElement.getAsJsonObject());
+                }
+            } catch (IOException e) {
+                System.err.println("Error reading the JSON file: " + e.getMessage());
+            }
+        }
+        jsonfile.add(newSong);
+        try (Writer writer = Files.newBufferedWriter(filePath)) {
+            gson.toJson(jsonfile, writer);
+            System.out.println("Song added successfully!");
+        } catch (IOException e) {
+            System.err.println("Error writing the JSON file: " + e.getMessage());
+        }
+
+    }
+    public void initialisePlaylist(Playlist playlist) throws FileNotFoundException {
+        Scanner sc = new Scanner(playlist.getPlaylist());
+        List<Song> songs = new ArrayList<>();
+    }
+}
