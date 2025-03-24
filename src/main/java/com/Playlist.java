bdadba0f0a12b1d577949379a758686895124cab
thinkbin006 <parvg06@gmail.com>
2025-03-24 16:08:40 +0530

    Final code Upload

--- src/main/java/com/Playlist.java
+++ src/main/java/com/Playlist.java
@@ -1,23 +1,24 @@
-package com;
+package org.sample.sampleGUI;
 
 import java.io.File;
 
 public class Playlist {
     private File playlist;
     private String name;
-
+    
+    
+    public String toString() {
+        return name;
+    }
     public void setName(String name) {
         this.name = name;
     }
-
     public File getPlaylist() {
         return playlist;
     }
-
     public String getName() {
         return name;
     }
-
     public void setPlaylist(File playlist) {
         this.playlist = playlist;
     }
